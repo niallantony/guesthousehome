10eff5045c893d8cd6eb11ba02704e61069964b1
niallantony <88824327+niallantony@users.noreply.github.com>
2023-06-19 18:54:45 +0900

    Clean up output

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,8 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: {
     index: './src/index.js',
-    initial: './src/initial.js',
-    header: './src/header.js',
-    ourstory: './src/ourstory.js',
-    enjoystay: './src/enjoystay.js',
-    explore: './src/explore.js',
   },
   module: {
     rules: [
@@ -19,14 +13,13 @@ module.exports = {
       },
     ],
   },
-  devtool: 'inline-source-map',
   plugins: [
     new HTMLWebpackPlugin({
       title: 'Guest House Home Page',
     })
   ],
   output: {
-    filename: '[name].main.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
